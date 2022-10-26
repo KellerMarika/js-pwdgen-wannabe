36835091548b86cb22a4416fa7e2552162318c43
KellerMarika <marikakeller92@gmail.com>
2022-10-26 16:11:40 +0200

    var css cambia colore con Js Funziona!!

--- js/main.js
+++ js/main.js
@@ -1,8 +1,9 @@
 
 
-const userName = prompt("nome");
-const userSurname = prompt("cognome");
-const userColor = prompt("colore preferito");
+const userName = prompt("Nome");
+const userSurname = prompt("Cognome");
+const userColor = prompt("colore: ( ATTENZIONE!! es: nome/ #esadecimale/ rgb/ hls)");
+
 
 
 const userCompleteName = `${userName} ${userSurname}`;
@@ -17,3 +18,7 @@ console.log("valore di userPassword", userPassword);
 
 document.getElementById("pass-generator").innerHTML =`<div class="text-uppercase">congratulazioni ${userCompleteName}!!! </div>
 la tua nuova username è: ${userName}${userSurname}${userColor}21`;
+
+
+/* CAMBIA COLORE IN BASE A QUANTO SCRIVE L'UTENTE!!!! */
+document.getElementById("pass-generator").style.setProperty('--color', userColor)
